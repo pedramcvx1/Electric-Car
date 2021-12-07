41fa4b4bd954b1bbd13b066d952fef2ea6080ecc
Mohammad Lotfi <87372623+pedramcvx1@users.noreply.github.com>
2021-12-07 19:51:52 +0200

    Add files via upload
    TMC4671 Library

---
+++ TMC4671/Definitions.h
@@ -0,0 +1,18 @@
+/*
+ * Definitions.h
+ *
+ *  Created on: 15.02.2018
+ *      Author: ED
+  */
+
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+	#include <stdlib.h>
+	#include <stdint.h>
+	#include <avr/io.h>
+	#include <avr/interrupt.h>
+
+	#include "TMC4671_Register.h"
+
+#endif // DEFINITIONS_H
---
+++ TMC4671/SysTick.h
@@ -0,0 +1,16 @@
+/*
+ * SysTick.h
+ *
+ *  Created on: 15.02.2018
+ *      Author: ED
+  */
+
+#ifndef SYSTICK_H
+#define SYSTICK_H
+
+	#include "Definitions.h"
+
+	void systick_init(void);
+	unsigned int systick_getTimer(void);
+
+#endif
